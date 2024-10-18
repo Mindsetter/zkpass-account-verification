1073e5f8ac2265421cfd9e2af289931be0c9c80b
Francis <ebo8630@gmail.com>
2024-10-18 21:07:43 +0000

    update appID and SchemaID

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -75,10 +75,10 @@ declare global {
 
 export default function Home() {
   const [appid1, setAppid1] = useState<string>(
-    "ca2ec33f-0b95-41a6-923f-1a317051e30b"
+    "c5168e8d-16ed-4301-aaea-25058673130d"
   );
   const [value1, setValue1] = useState<string>(
-    "aff0125569e64aa79838465891440287"
+    "ccaea2479b0440a797a2334475f456f8"
   );
   const [result, setResult] = useState<any>();
   const [attestAtationTx, setAttestAtationTx] = useState<string>();
